8b287b1dc1959dc5960f5e2b3afe5348434c44f1
ViniciosLugli <vinicioslugli@gmail.com>
2023-11-17 15:29:48 -0300

    feat(project): :sparkles: Upload p1 project and readme with setup and examples

---
+++ README.md
@@ -0,0 +1,27 @@
+# 2023-2B-T2-M8-A1
+
+## Customer Service Chatbot
+
+This is a simple Python program that implements a customer service chatbot using natural language processing and regular expressions to identify user intentions and provide automated responses to their questions. The program runs in the terminal, processing user inputs (commands) and displaying appropriate responses.
+
+## Getting Started
+
+### Requirements
+
+-   Python 3.7 or higher
+
+### Cloning the Repository
+
+```bash
+git clone git@github.com:ViniciosLugli/2023-2B-T2-M8-A1.git
+```
+
+### Running the Program
+
+```bash
+python3 main.py
+```
+
+## Example outputs:
+
+![Demo](https://i.imgur.com/RtZsREx.png)
---
+++ main.py
@@ -0,0 +1,67 @@
+import re
+
+
+class Vault:
+    INTENTS = {
+        r'\b(atualizar|alterar|atualização|forma de pagamento|informações de pagamento)\b': 'PaymentIntent',
+        r'\b(status|acompanhar|rastrear|pedido)\b': 'OrderIntent'
+
+    }
+
+    ACTIONS = {
+        'PaymentIntent': 'Para atualizar suas informações de pagamento, acesse a página de configurações de pagamento em nosso site.',
+        'OrderIntent': 'Para acompanhar o status do seu pedido, faça login em sua conta e acesse a área de pedidos do nosso site.'
+    }
+
+
+class Intent:
+    def __init__(self, name, action):
+        self.name = name
+        self.action = action
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class IntentRecognizer:
+    def __init__(self, intents):
+        self.intents = intents
+
+    def recognize(self, text):
+        for intent, name in self.intents.items():
+            if re.search(intent, text, re.IGNORECASE):
+                return Intent(name, text)
+
+        return None
+
+
+class IntentHandler:
+    def __init__(self, intents):
+        self.intents = intents
+
+    def handle(self, intent):
+        if intent.name in self.intents:
+            return self.intents[intent.name]
+
+        return None
+
+
+def main():
+    intents = Vault.INTENTS
+    actions = Vault.ACTIONS
+
+    intent_recognizer = IntentRecognizer(intents)
+    intent_handler = IntentHandler(actions)
+
+    while True:
+        text = input('>> ')
+
+        if intent := intent_recognizer.recognize(text):
+            action = intent_handler.handle(intent)
+            print(action)
+        else:
+            print('Não entendi o que você disse.')
+
+
+if __name__ == '__main__':
+    main()
